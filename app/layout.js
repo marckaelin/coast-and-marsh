741dd25272ccf20d8903e04c244d4d954e6dedd3
marckaelin <marc.kaelin@kingtidetechnologypartners.com>
2026-09-22 18:39:30 -0400

    Install Google Ads base tag

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import Script from 'next/script';
 
 export const metadata = {
   title: 'Coast & Marsh Insurance Advisory',
@@ -9,6 +10,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=AW-18468447688" strategy="afterInteractive" />
+      <Script id="google-ads-tag" strategy="afterInteractive">{`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'AW-18468447688');`}</Script>
     </html>
   );
 }
